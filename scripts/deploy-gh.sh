1a310b44f1ff3d0422a408fe7b3d3338b75467e0
WangSongTao21.github.io <1292845375@qq.com>
2019-06-06 17:18:11 +0800

    update

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -23,7 +23,7 @@ git add -A
 git commit -m 'update'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:WangSongTao21/WangSongTao21.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 #git push -f git@git.coding.net:aqazzz/Finen.git master
